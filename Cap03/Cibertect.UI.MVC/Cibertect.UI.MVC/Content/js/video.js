8815f238e4551b09a0468319d25d9ff5d220d44b
Astrid-Carrasco <astrid.carrasco.jesus@gmail.com>
2018-12-30 14:53:25 -0500

    cambios
    cambios

---
+++ Cap03/Cibertect.UI.MVC/Cibertect.UI.MVC/Content/js/video.js
@@ -0,0 +1,15 @@
+﻿(function (cibertec) {
+    cibertec.video =
+        {
+        play: function (videoID) {
+            var videoElement = document.getElementById(videoID);
+               videoElement.play();
+        },
+
+        pause: function (videoID) {
+            var videoElement = document.getElementById(videoID);
+            videoElement.pause();
+        }
+
+        }
+})(window.cibertec = window.cibertec || {}); 
