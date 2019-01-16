277ec7d16ae04a84d1a5ff1e0824e6390407bd23
leon355113 <jose.leon.dx@gmail.com>
2019-01-15 23:17:41 -0500

    cambio 15012019 23:17
    cambio 15012019 23:17

--- Cap03/Cibertect.UI.MVC/Cibertect.UI.MVC/Content/js/helpers.js
+++ Cap03/Cibertect.UI.MVC/Cibertect.UI.MVC/Content/js/helpers.js
@@ -1,15 +1,15 @@
 ﻿(function (cibertec) {
     cibertec.helpers =
         {
-            replaceAll : function( string , find, replace) {
+            replaceAll: function (string, find, replace) {
                 var result = string.replace(
-                    new RegExp(find,'g'),replace
+                    new RegExp(find, 'g'), replace
                 );
 
                 return result;
             },
 
-            getAniosArray : function (anioInicio) {
+            getAniosArray: function (anioInicio) {
                 var hoy = new Date();
                 var anios = [];
                 for (i = anioInicio; i <= hoy.getFullYear(); i++) {
@@ -19,7 +19,7 @@
                 return anios;
             },
 
-            BloquearControls: function(){
+            BloquearControls: function () {
                 $("input,select,button,textarea").attr("disabled", "disabled");
             },
 
@@ -38,6 +38,142 @@
                 console.log(typeof json); //string
 
                 return JSON.parse(json);
-            }
+            },
+            //funciones adicionales
+            //validar formato de fecha
+            validarFormatoFecha: function (campo) {
+                var RegExPattern = /^\d{1,2}\/\d{1,2}\/\d{2,4}$/;
+                if ((campo.match(RegExPattern)) && (campo != '')) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            existeFecha: function (fecha) {
+                var fechaf = fecha.split("/");
+                var day = fechaf[0];
+                var month = fechaf[1];
+                var year = fechaf[2];
+                var date = new Date(year, month, '0');
+                if ((day - 0) > (date.getDate() - 0)) {
+                    return false;
+                }
+                return true;
+            },
+           existeFecha2: function (fecha) {
+            var fechaf = fecha.split("/");
+            var d = fechaf[0];
+            var m = fechaf[1];
+            var y = fechaf[2];
+            return m > 0 && m < 13 && y > 0 && y < 32768 && d > 0 && d <= (new Date(y, m, 0)).getDate();
+          },
+           //---------------------validar RUC-------------------------------------//
+            //Handler para el evento cuando cambia el input
+            //Elimina cualquier caracter espacio o signos habituales y comprueba validez
+        validarInput: function (input) {
+        var ruc = input.value.replace(/[-.,[\]()\s]+/g, ""),
+            resultado = document.getElementById("resultado"),
+            existente = document.getElementById("existente"),
+            valido;
+
+        existente.innerHTML = "";
+
+        //Es entero?    
+        if ((ruc = Number(ruc)) && ruc % 1 === 0
+            && rucValido(ruc)) { // ⬅️ ⬅️ ⬅️ ⬅️ Acá se comprueba
+            valido = "Válido";
+            resultado.classList.add("ok");
+            obtenerDatosSUNAT(ruc);
+        } else {
+            valido = "No válido";
+            resultado.classList.remove("ok");
         }
+
+        resultado.innerText = "RUC: " + ruc + "\nFormato: " + valido;
+    },
+
+    // Devuelve un booleano si es un RUC válido
+    // (deben ser 11 dígitos sin otro caracter en el medio)
+    rucValido:function (ruc) {
+        //11 dígitos y empieza en 10,15,16,17 o 20
+        if (!(ruc >= 1e10 && ruc < 11e9
+            || ruc >= 15e9 && ruc < 18e9
+            || ruc >= 2e10 && ruc < 21e9))
+            return false;
+
+        for (var suma = -(ruc % 10 < 2), i = 0; i < 11; i++ , ruc = ruc / 10 | 0)
+            suma += (ruc % 10) * (i % 7 + (i / 7 | 0) + 1);
+        return suma % 11 === 0;
+
+    },
+
+    //Buscar datos del RUC y si existe
+    obtenerDatosSUNAT: function (ruc) {
+        //Init
+        var url = "https://cors-anywhere.herokuapp.com/wmtechnology.org/Consultar-RUC/?modo=1&btnBuscar=Buscar&nruc=" + ruc,
+            existente = document.getElementById("existente"),
+            xhr = false;
+        if (window.XMLHttpRequest) //Crear XHR
+            xhr = new XMLHttpRequest();
+        else if (window.ActiveXObject)
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        else return false;
+        //handler para respuesta
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) { //200 OK
+                var doc = document.implementation.createHTMLDocument()
+                    .documentElement,
+                    res = "",
+                    txt, campos,
+                    ok = false;
+
+                doc.innerHTML = xhr.responseText;
+                //Sólo el texto de las clases que nos interesa
+                campos = doc.querySelectorAll(".list-group-item");
+                if (campos.length) {
+                    for (txt of campos)
+                        res += txt.innerText + "\n";
+                    //eliminar blancos por demás
+                    res = res.replace(/^\s+\n*|(:) *\n| +$/gm, "$1");
+                    //buscar si está el texto "ACTIVO" en el estado
+                    ok = /^Estado: *ACTIVO *$/m.test(res);
+                } else
+                    res = "RUC: " + ruc + "\nNo existe.";
+
+                //mostrar el texto formateado
+                if (ok)
+                    existente.classList.add("ok");
+                else
+                    existente.classList.remove("ok");
+                existente.innerText = res;
+            }
+        } //falta verificar errores en conexión
+        xhr.open("POST", url, true);
+        xhr.send(null);
+    },
+ //---------------------valida dni//
+        nif: function  (dni) {
+        var numero
+        var letr
+        var letra
+        var expresion_regular_dni
+
+        expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
+
+        if (expresion_regular_dni.test(dni) == true) {
+            numero = dni.substr(0, dni.length - 1);
+            letr = dni.substr(dni.length - 1, 1);
+            numero = numero % 23;
+            letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
+            letra = letra.substring(numero, numero + 1);
+            if (letra != letr.toUpperCase()) {
+                alert('Dni erroneo, la letra del NIF no se corresponde');
+            } else {
+                alert('Dni correcto');
+            }
+        } else {
+            alert('Dni erroneo, formato no válido');
+        }
+    }
+}
 })(window.cibertec = window.cibertec || {}); 
