59e29d4c773cf8bfb71df6b7badda0fec16e5768
leon355113 <jose.leon.dx@gmail.com>
2019-01-17 00:05:46 -0500

    cambios 17012018 00:05
    cambios 17012018 00:05

--- Cap03/Cibertect.UI.MVC/Cibertect.UI.MVC/Content/js/helpers.js
+++ Cap03/Cibertect.UI.MVC/Cibertect.UI.MVC/Content/js/helpers.js
@@ -183,7 +183,35 @@
                 //Usando la definición del DOM level 2, "return" NO funciona.
                 e.preventDefault();
             }
+        },
+
+        ValidarTJ: function (numero_tarjeta) {
+        var cadena = numero_tarjeta.toString();
+        var longitud = cadena.length;
+        var cifra = null;
+        var cifra_cad = null;
+        var suma = 0;
+        for (var i = 0; i < longitud; i += 2) {
+            cifra = parseInt(cadena.charAt(i)) * 2;
+            if (cifra > 9) {
+                cifra_cad = cifra.toString();
+                cifra = parseInt(cifra_cad.charAt(0)) +
+                    parseInt(cifra_cad.charAt(1));
+            }
+            suma += cifra;
         }
+        for (var i = 1; i < longitud; i += 2) {
+            suma += parseInt(cadena.charAt(i));
+        }
+
+        if ((suma % 10) == 0) {
+            alert("Número de tarjeta correcto");
+        } else {
+            alert("El número de tarjeta no es válido");
+        }
+        },
+//------------------------------valida en JQuiery------------------------------------------//
+
 
 
 }
