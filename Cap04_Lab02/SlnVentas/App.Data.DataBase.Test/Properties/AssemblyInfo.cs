5915e53c161571e36fd92ca92dfe5fbe0dddd522
leon355113 <jose.leon.dx@gmail.com>
2019-01-20 14:22:35 -0500

    cambios 20012019 14:22
    cambios 20012019 14:22

---
+++ Cap04_Lab02/SlnVentas/App.Data.DataBase.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.Data.DataBase.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.Data.DataBase.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f120946-2bb9-46c3-82f6-d2998adbea00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
