d72c5bfc3efed6618e80e013a74e0d2f35e73fa8
leon355113 <jose.leon.dx@gmail.com>
2019-01-27 13:48:40 -0500

    cambios 27012019 13:48
    cambios 27012019 13:48

--- Cap04_Lab02/SlnVentas/App.Data.Repository/AppUnitOfWork.cs
+++ Cap04_Lab02/SlnVentas/App.Data.Repository/AppUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using App.Data.Repository.Interfaces;
+﻿using App.Data.DataBase;
+using App.Data.Repository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,13 +9,24 @@ using System.Threading.Tasks;
 
 namespace App.Data.Repository
 {
-    public class AppUnitOfWork : IAppUnitOfWork
+    public class AppUnitOfWork : IAppUnitOfWork, IDisposable
     {
         private readonly DbContext _context;
+
+        public AppUnitOfWork()
+        {
+            _context = new AppModel();
+            CreateRepositories();
+        }
 
         public AppUnitOfWork(DbContext context)
         {
             _context = context;
+            CreateRepositories();
+        }
+
+        private void CreateRepositories()
+        {
             this.CategoriaRepository = new CategoriaRepository(_context);
         }
 
